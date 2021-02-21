3e4b0c2c76fc055a8447b21a867e667eb9132e8b
santiago <santiagomed93@gmail.com>
2021-02-20 21:53:54 -0500

    first commit

---
+++ src/main/java/com/santiagomed93/demo/WebClientApplication.java
@@ -0,0 +1,13 @@
+package com.santiagomed93.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/santiagomed93/demo/WebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiagomed93.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
